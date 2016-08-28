4bb02eb2dbfa27de0e10ddcc0fe3c081362b29fd
Nicolas <nicolas.jouanin@gmail.com>
2016-08-28 17:23:45 +0200

    Unused

--- backend/src/main/resources/db/migration/V1__create_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "accounts" (
-  "account_id" VARCHAR(100) NOT NULL PRIMARY KEY,
-  "login" VARCHAR(100) NOT NULL,
-  "passwordHash" VARCHAR(100) NOT NULL,
-  "email" VARCHAR(100) NOT NULL,
-  "createdOn" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  "status" VARCHAR(100) NOT NULL);
-
