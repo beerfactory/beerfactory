d611220e13a8b0b2cf47a18ec1a0f1d728b5e076
Nicolas <nicolas.jouanin@gmail.com>
2016-10-26 21:45:49 +0200

    Migrate SignUpController to REST

--- server/app/controllers/SignUpController.scala
+++ server/app/controllers/SignUpController.scala
@@ -1,101 +1,99 @@
+/*
+ *********************************************************************************
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <nico@beerfactory.org> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Nicolas JOUANIN
+ *********************************************************************************
+ */
 package controllers
 
-import java.util.UUID
 import javax.inject.Inject
 
-import com.mohiva.play.silhouette.api._
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 import com.mohiva.play.silhouette.api.services.AvatarService
 import com.mohiva.play.silhouette.api.util.PasswordHasherRegistry
-import com.mohiva.play.silhouette.impl.providers._
-import forms.SignUpForm
+import com.mohiva.play.silhouette.api.{LoginInfo, SignUpEvent, Silhouette}
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import controllers.api.Bad
+import controllers.auth.api.{SignUp, Token}
 import models.auth.services.{AuthTokenService, UserService}
 import play.api.i18n.{I18nSupport, Messages, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits._
 import play.api.libs.mailer.{Email, MailerClient}
 import play.api.mvc.Controller
-import play.libs.Json
+import play.api.libs.json._
 import utils.auth.DefaultEnv
 
 import scala.concurrent.Future
 
-/**
-  * The `Sign Up` controller.
-  *
-  * @param messagesApi            The Play messages API.
-  * @param silhouette             The Silhouette stack.
-  * @param userService            The user service implementation.
-  * @param authInfoRepository     The auth info repository implementation.
-  * @param authTokenService       The auth token service implementation.
-  * @param avatarService          The avatar service implementation.
-  * @param passwordHasherRegistry The password hasher registry.
-  * @param mailerClient           The mailer client.
-  */
 class SignUpController @Inject()(
-                                  val messagesApi: MessagesApi,
-                                  silhouette: Silhouette[DefaultEnv],
-                                  userService: UserService,
-                                  authInfoRepository: AuthInfoRepository,
-                                  authTokenService: AuthTokenService,
-                                  avatarService: AvatarService,
-                                  passwordHasherRegistry: PasswordHasherRegistry,
-                                  mailerClient: MailerClient)
-  extends Controller with I18nSupport {
+    val messagesApi: MessagesApi,
+    silhouette: Silhouette[DefaultEnv],
+    userService: UserService,
+    authInfoRepository: AuthInfoRepository,
+    authTokenService: AuthTokenService,
+    avatarService: AvatarService,
+    passwordHasherRegistry: PasswordHasherRegistry,
+    mailerClient: MailerClient)
+    extends Controller
+    with I18nSupport {
 
-  /**
-    * Views the `Sign Up` page.
-    *
-    * @return The result to display.
-    */
-  def view = silhouette.UnsecuredAction.async { implicit request =>
-    Future.successful(Ok(views.html.signUp(SignUpForm.form)))
-  }
+  def signUp = silhouette.UnsecuredAction.async(parse.json) {
+    implicit request =>
+      request.body
+        .validate[SignUp]
+        .map { signUp ⇒
+          val loginInfo = LoginInfo(CredentialsProvider.ID, signUp.email)
+          userService.retrieve(loginInfo).flatMap {
+            case None =>
+              val authInfo =
+                passwordHasherRegistry.current.hash(signUp.password)
+              val fullName = (signUp.firstName, signUp.lastName) match {
+                case (Some(f), Some(l)) => Some(f + " " + l)
+                case (Some(f), None) => Some(f)
+                case (None, Some(l)) => Some(l)
+                case _ => None
+              }
+              for {
+                avatar <- avatarService.retrieveURL(signUp.email)
+                user <- userService.save(loginInfo,
+                                         false,
+                                         Some(signUp.email),
+                                         signUp.firstName,
+                                         signUp.lastName,
+                                         fullName,
+                                         avatar)
+                authInfo <- authInfoRepository.add(loginInfo, authInfo)
+                authToken <- authTokenService.create(user.userId)
+                result <- Future.successful(
+                  Ok(Json.toJson(Token(authToken.expiry))))
+              } yield {
+                val url = routes.ActivateAccountController
+                  .activate(authToken.tokenId)
+                  .absoluteURL()
+                mailerClient.send(
+                  Email(
+                    subject = Messages("email.sign.up.subject"),
+                    from = Messages("email.from"),
+                    to = Seq(signUp.email),
+                    bodyText = Some(views.txt.emails.signUp(user, url).body),
+                    bodyHtml = Some(views.html.emails.signUp(user, url).body)
+                  ))
 
-  /**
-    * Handles the submitted form.
-    *
-    * @return The result to display.
-    */
-  def submit = silhouette.UnsecuredAction.async { implicit request =>
-    SignUpForm.form.bindFromRequest.fold(
-      form => Future.successful(BadRequest(views.html.signUp(form))),
-      data => {
-        val result = Redirect(routes.SignUpController.view()).flashing("info" -> Messages("sign.up.email.sent", data.email))
-        val loginInfo = LoginInfo(CredentialsProvider.ID, data.email)
-        userService.retrieve(loginInfo).flatMap {
-          case Some(user) =>
-            val url = routes.SignInController.view().absoluteURL()
-            mailerClient.send(Email(
-              subject = Messages("email.already.signed.up.subject"),
-              from = Messages("email.from"),
-              to = Seq(data.email),
-              bodyText = Some(views.txt.emails.alreadySignedUp(user, url).body),
-              bodyHtml = Some(views.html.emails.alreadySignedUp(user, url).body)
-            ))
-
-            Future.successful(result)
-          case None =>
-            val authInfo = passwordHasherRegistry.current.hash(data.password)
-            for {
-              avatar <- avatarService.retrieveURL(data.email)
-              user <- userService.save(loginInfo, false, Some(data.email), Some(data.firstName), Some(data.lastName), Some(data.firstName + " " + data.lastName), avatar)
-              authInfo <- authInfoRepository.add(loginInfo, authInfo)
-              authToken <- authTokenService.create(user.userId)
-            } yield {
-              val url = routes.ActivateAccountController.activate(authToken.tokenId).absoluteURL()
-              mailerClient.send(Email(
-                subject = Messages("email.sign.up.subject"),
-                from = Messages("email.from"),
-                to = Seq(data.email),
-                bodyText = Some(views.txt.emails.signUp(user, url).body),
-                bodyHtml = Some(views.html.emails.signUp(user, url).body)
-              ))
-
-              silhouette.env.eventBus.publish(SignUpEvent(user, request))
-              result
-            }
+                silhouette.env.eventBus.publish(SignUpEvent(user, request))
+                result
+              }
+            case Some(user) =>
+              /* User already exists */
+              Future.successful(
+                Conflict(Json.toJson(Bad("signUp.user.alreadyExist"))))
+          }
         }
-      }
-    )
+        .recoverTotal {
+          case error =>
+            Future.successful(
+              BadRequest(Json.toJson(Bad(JsError.toJson(error).toString()))))
+        }
   }
 }
