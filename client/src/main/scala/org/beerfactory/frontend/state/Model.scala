342817417b98b0364533e345f03e1582f44576b7
Nicolas <nicolas.jouanin@gmail.com>
2016-11-16 23:02:16 +0100

    Add UserLogin actions management

--- client/src/main/scala/org/beerfactory/frontend/state/Model.scala
+++ client/src/main/scala/org/beerfactory/frontend/state/Model.scala
@@ -9,21 +9,47 @@
 package org.beerfactory.frontend.state
 
 import diode.data.Pot
-import diode.{Action, ActionHandler, ModelRW}
-import org.beerfactory.shared.api.UserInfo
+import diode.{Action, ActionHandler, Effect, ModelRW}
+import org.beerfactory.frontend.utils.AjaxApiFacade
+import org.beerfactory.shared.api.{ApiError, UserCurrentResponse, UserInfo}
 
 // Application model
-case class RootModel(userModel: UserModel)
+case class RootModel(userModel: UserModel, lastError: Option[ApiError])
 
 case class UserModel(isAuthentified: Boolean = false,
                      userInfo: Option[UserInfo] = None,
                      authToken: Pot[String] = Pot.empty)
 
 // Actions
-case class SetAuthToken(token: String) extends Action
+case class UserLogin(token: String)        extends Action
+case class SetUserInfo(userInfo: UserInfo) extends Action
+
+case class SetError(error: ApiError) extends Action
 
 class UserModelHandler[M](modelRW: ModelRW[M, UserModel]) extends ActionHandler(modelRW) {
   override def handle = {
-    case SetAuthToken(token) => updated(value.copy(authToken = value.authToken.ready(token)))
+    case UserLogin(token) =>
+      AppCircuit.storeAuthToken(token)
+      println("User login")
+      updated(value.copy(isAuthentified = true, authToken = value.authToken.ready(token)),
+              UserEffects.getUserInfo())
+    case SetUserInfo(info) ⇒
+      println(s"User info: $info")
+      updated(value.copy(userInfo = Some(info)))
+  }
+}
+
+//Effects
+object UserEffects {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def getUserInfo() = {
+    Effect(
+      AjaxApiFacade.getCurrentUser.map(
+        r ⇒
+          r.fold(
+            error ⇒ SetError(error),
+            response ⇒ SetUserInfo(response.userInfo)
+        )))
   }
 }
